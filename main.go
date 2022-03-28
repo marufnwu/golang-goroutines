ef4634d71968a6945923f002c1a548eb13c2c3a3
Maruf-Asus <dev00.maruf@gmal.com>
2022-03-28 19:16:05 +0600

    handle goroutine async memory access

--- main.go
+++ main.go
@@ -13,16 +13,22 @@ func main() {
 	wg.Add(gr*2)
 
 	var n int = 0
+	var m sync.Mutex
+	
 
 	for i:=0; i<gr; i++{
 		go func ()  {
 			time.Sleep(time.Second/10)
+			m.Lock()
 			n++
+			m.Unlock()
 			wg.Done()
 		}()
 
 		go func ()  {
 			time.Sleep(time.Second/10)
+			m.Lock()
+			defer m.Unlock() //it will execute end of this groutine cause i used defer function
 			n--
 			wg.Done()
 		}()
